6de2862514f0b25a4bcea8fc411176db23782f17
davikaro <davikaro@hotmail.com>
2024-11-14 13:38:18 -0300

    Conexão com websocket e novas páginas adicionadas ao cliente

--- src/communitylogic.js
+++ src/communitylogic.js
@@ -10,8 +10,8 @@ isPreCreated
 */
 
 // TODO Implement get communities
-function getCommunities() {
-    
+function getCommunities(ws) {
+
 }
 
 function renderCommunityPage(selectedCommunity) {
@@ -29,6 +29,8 @@ function renderCommunityList(commlist) {
 
 function updateCommunityList(communitylist) {
     // Update list
+
+    // Implement server side check, and add to array
     const updatedCommunityList = "yet to implement";
     // Then
     renderCommunityList(updatedCommunityList);
@@ -37,18 +39,44 @@ function updateCommunityList(communitylist) {
 
 
 window.onload = () => {
-
+    /* Model
     const testCommunity = {
         ID_Community: 0,
         Community_name: "test",
         Creation_date: "2024-1-1",
         Theme: "among us",
         isPreCreated: true,
+    }*/
+    const ws = new WebSocket("ws://" + window.location.hostname + ":8080")
+    const commlist = [];
+
+    ws.onopen = (ev) => {
+        console.log("Websocket connection has opened.")
     }
 
-    const commlist = [testCommunity];
+    ws.onmessage = (ev) => {
+        if (ev.data.length < 3) {
+            // Fail safe
+            console.log("Received a invalid command: " + ev.data)
+            return;
+        }
 
-    // Test for now
+        const command = ev.data.substring(0, 3)
+        switch (command) {
+            // Community received data
+            case "crd":
+                console.log("crd yay");
+                break;
+            case "sys":
+                console.log("System message: " + ev.data.substring(4, ev.data.length));
+                break;
+            default:
+                console.log(command + " is invalid... but here is the message: " + ev.data)
+                break;
+        }
+    }
 
-    renderCommunityPage(testCommunity);
+
+
+
 }
