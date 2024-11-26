f472b526c40d677ab14e6c45ee1a11e44690c4c6
davikaro <davikaro@hotmail.com>
2024-11-26 15:50:04 -0300

    Correção filters.js (issue #6 no repositório PapoFantasma)

--- src/jsfolder/filters.js
+++ src/jsfolder/filters.js
@@ -1,5 +1,8 @@
 // Controla a exibição do menu de filtros
-document.getElementById('filter-img').addEventListener('click', function () {
-    const filtersContainer = document.getElementById('filtersContainer');
-    filtersContainer.style.display = filtersContainer.style.display === 'none' ? 'block' : 'none';
-});
+
+window.onload = () => {
+    document.getElementById('filter-img').addEventListener('click', function () {
+        const filtersContainer = document.getElementById('filtersContainer');
+        filtersContainer.style.display = filtersContainer.style.display === 'none' ? 'block' : 'none';
+    });
+}
